00a09f3e20dd1869b56c65032151baadac8eb977
asafall <asafall21@gmail.com>
2019-04-07 18:00:14 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Asaf Baibekov on 07/04/2019.
+//  Copyright © 2019 Asaf Baibekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
