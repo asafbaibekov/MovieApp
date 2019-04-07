0fa1d1b7150c35d9ad72489453348a96ebe08ab6
asafall <asafall21@gmail.com>
2019-04-07 20:58:07 +0300

    clean all Movie objects from core data if api fetch succeeded and save them on core data

--- MovieApp/ViewController.swift
+++ MovieApp/ViewController.swift
@@ -19,7 +19,16 @@ class ViewController: UIViewController {
 		if let data: Data = try? Data(contentsOf: url),
 			let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: [.allowFragments]),
 			let jsonArray = jsonResponse as? [[String: Any]] {
-			
+			if MovieCoreDataHandler.cleanDelete() {
+				jsonArray.forEach { (item) in
+					let title = item["title"] as! String
+					let image = URL(string: item["image"] as! String)!
+					let rating = item["rating"] as! Double
+					let releaseYear = item["releaseYear"] as! Int16
+					let genre = item["genre"] as! [String]
+					MovieCoreDataHandler.saveObeject(title: title, image: image, rating: rating, releaseYear: releaseYear, genre: genre)
+				}
+			}
 		}
 	}
 
