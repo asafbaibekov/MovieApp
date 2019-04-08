bc0471abc53abac11f8f7fe55d69ecda05ab48eb
asafall <asafall21@gmail.com>
2019-04-08 16:38:55 +0300

    add view controller for add movie with qr

---
+++ MovieApp/AddMovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddMovieViewController.swift
+//  MovieApp
+//
+//  Created by Asaf Baibekov on 08/04/2019.
+//  Copyright © 2019 Asaf Baibekov. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
